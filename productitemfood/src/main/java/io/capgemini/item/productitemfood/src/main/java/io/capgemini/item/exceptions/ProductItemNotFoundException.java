b912b0cc5685a4978dd0f0401f24888b285ea6c4
Madhu9182 <96116561+Madhu9182@users.noreply.github.com>
2022-09-02 12:55:16 +0530

    Delete productitemfood directory

--- productitemfood/src/main/java/io/capgemini/item/productitemfood/src/main/java/io/capgemini/item/exceptions/ProductItemNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package io.capgemini.item.exceptions;
-
-public class ProductItemNotFoundException extends Exception{
-    public ProductItemNotFoundException() {
-        super("Product Item Not Found");
-    }
-
-}
--- productitemfood/src/main/java/io/capgemini/item/repository/ProductItemRepository.java
+++
@@ -1,9 +0,0 @@
-package io.capgemini.item.repository;
-
-import io.capgemini.item.model.ProductItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductItemRepository extends JpaRepository<ProductItem, Long> {
-}
